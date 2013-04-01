a1f27af6a61d9f12343a53a2a09fcd6f3cf7cff3
Natraj Kaushik <natraj.iitb@gmail.com>
2013-03-31 22:15:26 -0400

    Added AisleViewActivity

---
+++ NutriNinja/src/com/example/nutrininja/AisleViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.nutrininja;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AisleViewActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
