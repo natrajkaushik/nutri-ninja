205a00af501add6099ffbc2ed40050155cecc2fd
s1m55r <sameervijaykar@gmail.com>
2013-04-01 21:20:17 -0400

    Implemented drawing of markers at custom position and with custom lengths.

--- NutriNinja/src/com/doe/nutrininja/AisleViewActivity.java
+++ NutriNinja/src/com/doe/nutrininja/AisleViewActivity.java
@@ -1,76 +1,88 @@
 package com.doe.nutrininja;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 
 public class AisleViewActivity extends Activity {
 	public static final String TAG = "AISLE_VIEW_ACTIVITY";
 	
+	private static final Animation BLINK_ANIMATION = new AlphaAnimation(1, 0);
+	static {
+		BLINK_ANIMATION.setDuration(500); 							// duration - half a second
+	    BLINK_ANIMATION.setInterpolator(new LinearInterpolator());	// do not alter BLINK_ANIMATION rate
+	    BLINK_ANIMATION.setRepeatCount(Animation.INFINITE);			// Repeat BLINK_ANIMATION infinitely
+	    BLINK_ANIMATION.setRepeatMode(Animation.REVERSE);				// Reverse BLINK_ANIMATION at the end
+	}
+	
+	private int leftAisleHeight = 0;
+	private int rightAisleHeight = 0;
+	private double densityFactor = 1f;
+	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		densityFactor = getResources().getDisplayMetrics().density;
+		
 		setContentView(R.layout.aisle_view);
-
-		ListView listView = (ListView) findViewById(R.id.brand_list);
 
 		String[] items = { "Turner Diary", "Publix", "American Diary",
 				"Parmalat", "Shurfresh", "Prairie Farms" };
-
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, android.R.id.text1, items);
-
-		// Assign adapter to ListView
+		ListView listView = (ListView) findViewById(R.id.brand_list);
 		listView.setAdapter(adapter);
 		
-		DisplayMetrics metrics = getResources().getDisplayMetrics();
-		Log.d(TAG, String.valueOf(metrics.densityDpi));
-		
-		View left_aisle_position_marker = findViewById(R.id.left_aisle_position_marker);
-		
-		// left_aisle_position_marker.setTop(50);
-		// left_aisle_position_marker.setLayoutParams(new FrameLayout.LayoutParams(100, 550));
-		//left_aisle_position_marker.setMinimumHeight(250);
-		left_aisle_position_marker.setVisibility(View.VISIBLE);
-		left_aisle_position_marker.setBackgroundColor(Color.RED);
-		left_aisle_position_marker.setPadding(0, 50, 0, 0);
-	
-		 //LayoutParams layoutParams = left_aisle_position_marker.getLayoutParams();
-		 //layoutParams.height = 200;
-		 //layoutParams.width = 50;
-		 //layoutParams.topMargin = 50;
-		 //left_aisle_position_marker.requestLayout();
-		 //left_aisle_position_marker.setLayoutParams(layoutParams);
-		 //left_aisle_position_marker.requestLayout();
-		
-		final Animation animation = new AlphaAnimation(1, 0); // Change alpha from fully visible to invisible
-	    animation.setDuration(500); // duration - half a second
-	    animation.setInterpolator(new LinearInterpolator()); // do not alter animation rate
-	    animation.setRepeatCount(Animation.INFINITE); // Repeat animation infinitely
-	    animation.setRepeatMode(Animation.REVERSE); // Reverse animation at the end so the button will fade back in
-		
-	    left_aisle_position_marker.setAnimation(animation);
-	}
-
-	protected void onResume() {
-		super.onResume();
-		
-		final View left_aisle = findViewById(R.id.left_aisle_rectangle);
-		left_aisle.post(new Runnable(){
-			public void run() {
-				Log.d(TAG, "width " + left_aisle.getMeasuredWidth());
-				Log.d(TAG, "height " + left_aisle.getMeasuredHeight());
+		final FrameLayout left_aisle_frame = (FrameLayout) findViewById(R.id.left_aisle);
+		left_aisle_frame.getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+			public void onGlobalLayout() {
+				Log.d(TAG, "Left height: " + left_aisle_frame.getHeight());
+				leftAisleHeight = (int) (left_aisle_frame.getHeight() / densityFactor);
 			}
 		});
 		
-		//Log.d(TAG, String.valueOf(left_aisle.getHeight()));
+		final FrameLayout right_aisle_frame = (FrameLayout) findViewById(R.id.right_aisle);
+		right_aisle_frame.getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+			public void onGlobalLayout() {
+				Log.d(TAG, "Right height: " + right_aisle_frame.getHeight());
+				rightAisleHeight = (int) (right_aisle_frame.getHeight() / densityFactor);
+			}
+		});
+		
+		View left_aisle_position_marker = findViewById(R.id.left_aisle_position_marker);
+		left_aisle_position_marker.setVisibility(View.VISIBLE);
+	    left_aisle_position_marker.setAnimation(BLINK_ANIMATION);
+		
+		View right_aisle_position_marker = findViewById(R.id.right_aisle_position_marker);
+		right_aisle_position_marker.setVisibility(View.VISIBLE);
+	    right_aisle_position_marker.setAnimation(BLINK_ANIMATION);
+	}
+	
+	protected void onResume() {
+		super.onResume();
+		
+		View topLevelView = findViewById(R.id.top_level_view);
+		topLevelView.getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+			public void onGlobalLayout() {
+				Log.d(TAG, "Redrawing markers...");
+				View left_aisle_position_marker = findViewById(R.id.left_aisle_position_marker);
+				View right_aisle_position_marker = findViewById(R.id.right_aisle_position_marker);
+			    redrawMarker(left_aisle_position_marker, leftAisleHeight, 0.30, 0.25);
+			    redrawMarker(right_aisle_position_marker, rightAisleHeight, 0.60, 0.30);
+			}
+		});
+	}
+
+	private void redrawMarker(View marker, int height, double posFraction, double lenFraction) {
+		FrameLayout.LayoutParams params = ((FrameLayout.LayoutParams) marker.getLayoutParams());
+		params.height = (int) ((height * lenFraction) * densityFactor);
+		params.topMargin = (int) ((height * posFraction) * densityFactor);
 	}
 }
